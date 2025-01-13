265da39d38e2c324bf236239080a0f3dfcfbbc28
syedmehedi34 <syedmeehdi34@gmail.com>
2025-01-13 23:51:58 +0600

    project setup

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+import NavBar from "../pages/Shared/NavBar";
+
+const Main = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <div className="w-11/12 mx-auto">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
+import Error from "../pages/Error";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "signup",
+        element: <SignUp></SignUp>,
+      },
+    ],
+  },
+]);
