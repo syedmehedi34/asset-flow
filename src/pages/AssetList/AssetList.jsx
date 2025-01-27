6e7fc0d4fa23f7191ead848be63cbbdb98776f10
syedmehedi34 <syedmehedi34@gmail.com>
2025-01-27 10:24:35 +0600

    asset list page changed

--- src/pages/AssetList/AssetList.jsx
+++ src/pages/AssetList/AssetList.jsx
@@ -151,7 +151,7 @@ const AssetList = () => {
                 </label>
                 <input
                   type="text"
-                  placeholder="Search by name or email..."
+                  placeholder="Search by asset name ..."
                   className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                   value={searchText}
                   onChange={(e) => setSearchText(e.target.value)}
