e620b25eee7a5d0a2390a0cb852528ad09677f62
syedmehedi34 <syedmeehdi34@gmail.com>
2025-05-05 16:23:29 +0600

    asset request send page updated

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-  // baseURL: "http://localhost:5002",
-  baseURL: "https://assetflow-livid.vercel.app",
+  baseURL: "http://localhost:5002",
+  // baseURL: "https://assetflow-livid.vercel.app",
 });
 const useAxiosSecure = () => {
   const navigate = useNavigate();
