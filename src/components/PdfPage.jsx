2e1009eebd89f49e39701868b6bd6e0168a036b1
syedmehedi34 <syedmehedi34@gmail.com>
2025-05-06 00:11:48 +0600

    my assets page redesigned

--- src/components/PdfPage.jsx
+++ src/components/PdfPage.jsx
@@ -12,7 +12,7 @@ function PdfPage({ asset }) {
       <PDFDownloadLink
         document={<AssetPdfDocument asset={asset} />}
         fileName="document.pdf"
-        className="inline-flex items-center px-4 py-2.5 bg-green-600 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 transition duration-300 ease-in-out transform hover:-translate-y-1"
+        className="inline-flex items-center px-4 py-2.5 bg-teal-600 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 transition duration-300 ease-in-out transform hover:-translate-y-1"
       >
         <>
           <FileDown className="w-5 h-5 mr-2" />
